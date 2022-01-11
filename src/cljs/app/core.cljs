b111d6d47a9c4adf3f1939a492506bf8cd7da0c8
Mitchell Horning <mhorning@ata-llc.com>
2022-01-11 14:29:14 -0500

    Downgrade reagent version until error can be replicated w/ newer

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,11 +1,22 @@
 (ns ^:figwheel-hooks app.core
   (:require
    [day8.re-frame.tracing :refer [fn-traced]]
+   [goog.dom :as gdom]
+   [reagent.dom :as rdom]
    [re-frame.core :as rf]))
 
+(rf/reg-event-db ::next
+ (fn-traced [_ _]
+            {}))
+
 (rf/reg-event-fx ::init
- (fn-traced [_ _] {}))
+ (fn-traced [_ _]
+            {:dispatch [::next]}))
+
+(defn mount-root []
+  ;; (rdom/render [:div "content"] (gdom/getElement "app"))
+  )
 
 (defn ^:export init []
-  (rf/dispatch [::init])
-  (rf/dispatch [::init]))
+  (mount-root)
+  (rf/dispatch-sync [::init]))
